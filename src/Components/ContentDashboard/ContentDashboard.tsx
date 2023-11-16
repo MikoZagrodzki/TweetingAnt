c39e1c1c1d2d2209f327ef934f93c4b175923244
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-11-16 19:00:45 +0000

    filtering logic fixed for content dashboard - tweets

--- src/Components/ContentDashboard/ContentDashboard.tsx
+++ src/Components/ContentDashboard/ContentDashboard.tsx
@@ -4,6 +4,8 @@ import getAllScrapedTweets from "../../Functionalities/GetAllScrapedTweets";
 import classnames from 'classnames';
 import { useAuth } from "../../AuthContext";
 import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
 
 
 interface Tweet {
@@ -23,9 +25,9 @@ function ContentDashboard() {
 
   const [tweets, setTweets] = useState<Tweet[] | []>([]);
   const [filteredTweets, setFilteredTweets] = useState<Tweet[] | []>([]);
-  const [filtersApplied, setFiltersApplied] = useState<boolean>(false);
-  const [personality, setPersonality] = useState<string>("");
-  const [tweetType, setTweetType] = useState<string>("");
+  const [searchPersonality, setSearchPersonality] = useState<string>("")
+  const [searchTweetType, setSearchTweetType] = useState<string>("")
+
 
   const[toggleUseEffectForTweets, setToggleUseEffectForTweets] = useState<boolean>(false);
 
@@ -37,47 +39,84 @@ function ContentDashboard() {
     setFilteredTweets(tweetsData);
   };
 
-  const handleApply = () => {
-    let result;
+
+  let personalitiesNoDuplicates = Array.from(new Set(tweets.map((tweet) => tweet.personality))).sort();
+  let tweetTypesNoDuplicates = Array.from(new Set(tweets.map((tweet) => tweet.tweettype))).sort();
+
+  let personalitiesWhenTweetTypeSelected = Array.from(new Set(
+    tweets
+      .filter((tweet) => tweet.tweettype === searchTweetType)
+      .map((tweet) => tweet.personality)
+      .filter((personality) => personality !== null)
+      .sort()
+  ));
+
+  let tweetTypesWhenPersonalitySelected = Array.from(new Set(
+    tweets
+      .filter((tweet) => tweet.personality === searchPersonality)
+      .map((tweet) => tweet.tweettype)
+      .filter((tweetType) => tweetType !== null)
+      .sort()
+  ));
+
+
+  const handlePersonalitySearch = (personalityToSearch: string) => {
+    setSearchPersonality(personalityToSearch);
   
-    if (personality !== "" && tweetType !== "") {
-      result = tweets.filter(
-        (tweet) =>
-          tweet.personality === personality && tweet.tweettype === tweetType
+    if (personalityToSearch !== "") {
+      const filtered = tweets.filter((tweet) =>
+        tweet.personality &&
+        tweet.personality.toLowerCase().includes(personalityToSearch.toLowerCase()) &&
+        (!searchTweetType || tweet.tweettype?.toLowerCase() === searchTweetType.toLowerCase())
       );
-      setFiltersApplied(true);
-    } else if (personality !== "") {
-      result = tweets.filter((tweet) => tweet.personality === personality);
-      setFiltersApplied(true);
-    } else if (tweetType !== "") {
-      result = tweets.filter((tweet) => tweet.tweettype === tweetType);
-      setFiltersApplied(true);
-    } else {
-      result = tweets;
-      setPersonality("");
-      setTweetType("");
-      setFiltersApplied(false);
-    }
   
-    setFilteredTweets(result);
+      setFilteredTweets(filtered.length > 0 ? filtered : []);
+    } else {
+      // If personalityToSearch is empty, apply only the tweetType filter
+      const tweetTypeFiltered = tweets.filter((tweet) =>
+        !searchTweetType || tweet.tweettype?.toLowerCase() === searchTweetType.toLowerCase()
+      );
+  
+      setFilteredTweets(tweetTypeFiltered.length > 0 ? tweetTypeFiltered : []);
+    }
   };
+  
+  const handleTweetTypeSearch = (tweetTypeToSearch: string) => {
+    setSearchTweetType(tweetTypeToSearch);
+  
+    if (tweetTypeToSearch !== "") {
+      const filtered = tweets.filter((tweet) =>
+        tweet.tweettype &&
+        tweet.tweettype.toLowerCase().includes(tweetTypeToSearch.toLowerCase()) &&
+        (!searchPersonality || tweet.personality?.toLowerCase() === searchPersonality.toLowerCase())
+      );
+  
+      setFilteredTweets(filtered.length > 0 ? filtered : []);
+    } else {
+      // If tweetTypeToSearch is empty, apply only the personality filter
+      const personalityFiltered = tweets.filter((tweet) =>
+        !searchPersonality || tweet.personality?.toLowerCase() === searchPersonality.toLowerCase()
+      );
+  
+      setFilteredTweets(personalityFiltered.length > 0 ? personalityFiltered : []);
+    }
+  };
+  
+ 
   
 
   const handleShowAll = () => {
-    setFiltersApplied(false);
     setFilteredTweets(tweets);
-    setPersonality("");
-    setTweetType("");
-  }
+    setSearchPersonality("");
+    setSearchTweetType("");
+  }  
 
   useEffect(() => {
     setTweets([]);
     setFilteredTweets([]);
     getTweets();
-  }, [toggleUseEffectForTweets]);
+  }, [toggleUseEffectForTweets]);  
 
-  const uniquePersonalities = Array.from(new Set(tweets.map(tweet => tweet.personality)));
-  const uniqueTweetTypes = Array.from(new Set(tweets.map(tweet => tweet.tweettype)));
 
 
   const BUTTON_STYLING =classnames('text-xs sm:text-sm whitespace-nowrap bg-secondary font-semibold px-1 rounded-sm border border-accent hover:bg-accent hover:text-white hover:border-primary shadow-md')
@@ -88,46 +127,61 @@ function ContentDashboard() {
     <div id='top' className="min-h-screen w-screen mt-10 mb-10 flex flex-col items-center gap-y-10">
       <a href="#top" className=" fixed z-20 bottom-10 right-10 font-extrabold text-2xl bg-highlight rounded-lg text-accent p-1 shadow-lg border border-accent hover:text-highlight hover:border-highlight hover:bg-accent">^</a>
       <h1 className="">Content Dashboard</h1>
-      {tweets.length >0 && 
+      {tweets.length > 0 && 
         <div className={`${INFO_TEXT} flex flex-col md:flex-row items-center gap-3 `}>
-          {tweets.filter(tweet => tweet.isapproved === 'pending')&&
-            <p>Tweets awaiting approval: {tweets.filter(tweet => tweet.isapproved === 'pending').length}</p>
+          {filteredTweets.filter(tweet => tweet.isapproved === 'pending')&&
+            <p>Tweets awaiting approval: {filteredTweets.filter(tweet => tweet.isapproved === 'pending').length}</p>
           }
-          {tweets.filter(tweet => tweet.isapproved === 'approved')&&
-            <p>Approved Tweets: {tweets.filter(tweet => tweet.isapproved === 'approved').length}</p>
+          {filteredTweets.filter(tweet => tweet.isapproved === 'approved')&&
+            <p>Approved Tweets: {filteredTweets.filter(tweet => tweet.isapproved === 'approved').length}</p>
           }
         </div>
       }
       <div id="selection_bar" className="flex flex-row gap-1 justify-center flex-wrap">
         <select
-          value={personality || ""}
-          onChange={(e) => setPersonality(e.target.value)}
+          value={searchPersonality}
+          onChange={(e) => handlePersonalitySearch(String(e.target.value))}
           className={BUTTON_STYLING}
         >
-          <option value="">Select Personality</option>
-          {uniquePersonalities.map(personalityValue => (
-            <option key={personalityValue} value={personalityValue || ''}>
-              {personalityValue}
-          </option>
-          ))}
+          <option value="">All Personalities</option>
+          {searchTweetType ==="" ?
+            personalitiesNoDuplicates.map(uniquePersonality => (
+              <option key={uuidv4()} value={uniquePersonality || ''}>
+                {uniquePersonality}
+              </option>
+            ))
+            :
+            personalitiesWhenTweetTypeSelected.map(uniquePersonality => (
+              <option key={uuidv4()} value={uniquePersonality || ''}>
+                {uniquePersonality}
+              </option>
+            ))
+          }
       </select>
       <select
-        value={tweetType || ""}
-        onChange={(e) => setTweetType(e.target.value)}
+        value={searchTweetType}
+        onChange={(e) => handleTweetTypeSearch(String(e.target.value))}
         className={BUTTON_STYLING}
       >
-        <option value="">Select Tweet Type</option>
-        {uniqueTweetTypes.map((uniqueTypes) => (
-          <option key={uniqueTypes} value={uniqueTypes || ""}>
-            {uniqueTypes}
-          </option>
-        ))}
+        <option value="">All Tweet Types</option>
+        {searchPersonality === "" ?
+          tweetTypesNoDuplicates.map((uniqueTweetType) => (
+            <option key={uniqueTweetType} value={uniqueTweetType || ""}>
+              {uniqueTweetType}
+            </option>
+          ))
+        :
+          tweetTypesWhenPersonalitySelected.map((uniqueTweetType) => (
+            <option key={uniqueTweetType} value={uniqueTweetType || ""}>
+              {uniqueTweetType}
+            </option>
+          ))
+        }
       </select>
-      <button className={BUTTON_STYLING} onClick={handleApply}>Apply</button>
       {filteredTweets.some((tweet) => tweet.isapproved === 'approved') &&
         <a href="#approvedTweets" className={`md:hidden ${BUTTON_STYLING}`}>Approved Tweets</a> 
       }
-      {filtersApplied && (
+      {(searchPersonality !== "" || searchTweetType !== "") && (
         <button className={BUTTON_STYLING} onClick={handleShowAll}>
           Show All
         </button>
@@ -187,9 +241,7 @@ function ContentDashboard() {
                     />)
               })}
               </ul>
-            )}
-
-            
+            )} 
           </div>
     </div>
   );
